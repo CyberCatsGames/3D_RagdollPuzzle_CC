cbbfe8914490db12776a608c31495298535ed54f
Yurius <59869999+Yurius-Softable@users.noreply.github.com>
2023-01-12 19:04:48 +0400

    Change Camera Path

--- Assets/_CyberCats/Scripts/CM_FinishEvent.cs
+++ Assets/_CyberCats/Scripts/CM_FinishEvent.cs
@@ -7,11 +7,19 @@ using UnityEngine;
 public class CM_FinishEvent : MonoBehaviour
 {
     public CinemachineDollyCart CM_Cart;
+    public float StartDelay = 1f;
 
+    private void Start() {
+        Invoke(nameof(StartPath), StartDelay);
+    }
     private void Update() {
         if(CM_Cart.m_Position >= 1f) {
             this.transform.parent = null;
             GetComponent<CameraController>().enabled = true;
         }
+    }
+
+    void StartPath() {
+        CM_Cart.m_Speed = 0.15f;
     }
 }

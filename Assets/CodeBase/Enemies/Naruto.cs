bd99ad40dfbe43128a2037ca96617a6d2f62bff5
Our_bundle_of_joy <76647473+Daniyal-Akhadov@users.noreply.github.com>
2023-01-13 20:07:01 +0400

    [add] tutor show and hide with block input

---
+++ Assets/CodeBase/Enemies/Naruto.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Enemies
+{
+    public class Naruto : MonoBehaviour
+    {
+    }
+}
---
+++ Assets/CodeBase/Enemies/Tutor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using ARP.APR.Scripts;
+using DG.Tweening;
+using UnityEngine;
+
+namespace CodeBase.Enemies
+{
+    public class Tutor : MonoBehaviour
+    {
+        [SerializeField] private float _duration = 1f;
+        [SerializeField] private CanvasGroup _canvasGroup;
+
+        private APRController _input;
+
+        private void Awake()
+        {
+            _input = FindObjectOfType<Naruto>().GetComponent<APRController>();
+        }
+
+        private void Start()
+        {
+            Invoke(nameof(Show), 1f);
+            Invoke(nameof(Close), 8f);
+        }
+
+        private void Show()
+        {
+            _input.useControls = false;
+            _canvasGroup.gameObject.SetActive(true);
+            _canvasGroup.alpha = 0f;
+            _canvasGroup.DOFade(1f, _duration);
+        }
+
+        public void Close()
+        {
+            _canvasGroup.DOFade(0f, 0.5f)
+                .OnComplete
+                (
+                    () =>
+                    {
+                        _canvasGroup.gameObject.SetActive(false);
+                        _input.useControls = true;
+                    }
+                );
+        }
+    }
+}

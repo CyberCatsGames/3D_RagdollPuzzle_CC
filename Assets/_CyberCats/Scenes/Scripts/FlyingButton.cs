b0dcb5853c42c882de82718771baf583b7c4020a
Our_bundle_of_joy <76647473+Daniyal-Akhadov@users.noreply.github.com>
2023-01-14 16:39:55 +0400

    [add] mouse show animation

--- Assets/_CyberCats/Scenes/Scripts/FlyingButton.cs
+++ Assets/_CyberCats/Scenes/Scripts/FlyingButton.cs
@@ -1,7 +1,11 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace _CyberCats.Scenes.Scripts
 {
+    [RequireComponent(typeof(Rigidbody))]
     [RequireComponent(typeof(AudioSource))]
     public class FlyingButton : MonoBehaviour
     {
@@ -14,18 +18,52 @@ namespace _CyberCats.Scenes.Scripts
 
         private AudioSource _audioSource;
         private static Vector3 _currentScale;
+        private static bool _isFirst = true;
+        private bool _canTouch;
+        private Rigidbody _rigidbody;
 
         private AudioClip RandomClip =>
             _audioClip[Random.Range(0, _audioClip.Length)];
 
         private void Awake()
         {
+            _rigidbody = GetComponent<Rigidbody>();
             _currentScale = transform.localScale;
             _audioSource = GetComponent<AudioSource>();
+
+            if (_isFirst == false)
+            {
+                _currentScale = Vector3.one * 0.44315f;
+                transform.localScale = Vector3.zero;
+                _canTouch = true;
+            }
+        }
+
+        private void Start()
+        {
+            if (_isFirst == true)
+            {
+                Invoke(nameof(DoScaleAnimation), 5f);
+            }
+        }
+
+        private void DoScaleAnimation()
+        {
+            transform.DOScale(_currentScale, 1f).OnComplete(() => _canTouch = true);
+            _isFirst = false;
+        }
+
+        private void OnMouseEnter()
+        {
+            _rigidbody.isKinematic = false;
+            _rigidbody.AddForceAtPosition(250f * Vector3.one, Camera.main.ScreenToWorldPoint(Input.mousePosition));
         }
 
         private void OnMouseDown()
         {
+            if (_canTouch == false)
+                return;
+
             for (int i = 0; i < _multiplierCount; i++)
             {
                 Instantiate(_prefab, transform.position, Quaternion.identity);
---
+++ Assets/_CyberCats/Scenes/Scripts/ShowObstacle.cs
@@ -0,0 +1,24 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace _CyberCats.Scenes.Scripts
+{
+    public class ShowObstacle : MonoBehaviour
+    {
+        [SerializeField] private float _duration = 4f;
+        
+        private Vector3 _finishScale;
+
+        private void Awake()
+        {
+            _finishScale = transform.localScale;
+            transform.localScale /= 2;
+        }
+
+        private void Start()
+        {
+            transform.DOScale(_finishScale, _duration)
+                .SetLoops(-1, LoopType.Yoyo);
+        }
+    }
+}

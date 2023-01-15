e9f3a2462f6dcb26c2581b28974fb3002f7b5573
Yurius <59869999+Yurius-Softable@users.noreply.github.com>
2023-01-15 17:32:26 +0400

    Add Win

--- Assets/CodeBase/TriggerObserver.cs
+++ Assets/CodeBase/TriggerObserver.cs
@@ -8,6 +8,8 @@ namespace CodeBase
         [SerializeField] private UnityEvent _enter;
         [SerializeField] private UnityEvent _stay;
         [SerializeField] private UnityEvent _exit;
+        [SerializeField] private UnityEvent _win;
+        [SerializeField] private UnityEvent _alone;
         
         [SerializeField] private LayerMask _layer;
 
@@ -17,17 +19,27 @@ namespace CodeBase
             {
                 _enter?.Invoke();
                 print("Enter");
+                if (other.gameObject.tag == "CanBeGrabbed") {
+                    print("Saske");
+                    Debug.Log(other.gameObject.ToString());
+                    _win?.Invoke();
+                }
+                else {
+                    print("alone");
+                    _alone?.Invoke();
+                }
+                
             }
         }
 
-        private void OnTriggerStay(Collider other)
-        {
-            if (other.gameObject.IsInLayer(_layer))
-            {
-                _stay?.Invoke();
-                print("Stay");
-            }
-        }
+        //private void OnTriggerStay(Collider other)
+        //{
+        //    if (other.gameObject.IsInLayer(_layer))
+        //    {
+        //        _stay?.Invoke();
+        //        print("Stay");
+        //    }
+        //}
 
         private void OnTriggerExit(Collider other)
         {

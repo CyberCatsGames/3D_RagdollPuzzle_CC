5e6e81cde34c636b4c802d579c665cdcefdd90a8
Our_bundle_of_joy <76647473+Daniyal-Akhadov@users.noreply.github.com>
2023-01-16 04:55:06 +0400

    [add] something ne more time

--- Assets/_CyberCats/Scripts/CY_VideoPlayer.cs
+++ Assets/_CyberCats/Scripts/CY_VideoPlayer.cs
@@ -1,10 +1,15 @@
-
+using System;
+using Animations;
+using ARP.APR.Scripts;
+using CodeBase.Enemies;
 using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.Events;
 using Unity.VisualScripting;
+using Random = UnityEngine.Random;
 
-public class CY_VideoPlayer : MonoBehaviour {
+public class CY_VideoPlayer : MonoBehaviour
+{
     [SerializeField] private VideoPlayer _videoPlayer;
     [SerializeField] private GameObject _bG_Url;
     [SerializeField] private GameObject _bG_Gif;
@@ -14,32 +19,75 @@ public class CY_VideoPlayer : MonoBehaviour {
     public bool DefaultUrl = true;
 
     public VideoClip[] clips;
+    private EnemyAnimator[] _enemies;
 
-    private void OnEnable() {
-        if (!DefaultUrl) {
-            _videoPlayer.clip = clips[Random.Range(0, clips.Length-1)];
+
+    private void Start()
+    {
+        _enemies = FindObjectsOfType<EnemyAnimator>();
+
+        Invoke(nameof(TurnOn), 1f);
+    }
+
+    private void TurnOn()
+    {
+        foreach (EnemyAnimator enemyAnimator in _enemies)
+        {
+            enemyAnimator.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (!DefaultUrl)
+        {
+            _videoPlayer.clip = clips[Random.Range(0, clips.Length - 1)];
         }
 
         _videoPlayer.loopPointReached += EndReached;
     }
-    private void Update() {
-        if (!_isPlaying) {
-            if (_videoPlayer.isPrepared) {
+
+    private void Update()
+    {
+        if (!_isPlaying)
+        {
+            if (_videoPlayer.isPrepared)
+            {
                 ShowVideo();
                 _bG_Url.SetActive(true);
                 _bG_Gif.SetActive(false);
-            } else {
+            }
+            else
+            {
                 _bG_Url.SetActive(false);
                 _bG_Gif.SetActive(true);
             }
         }
     }
 
-    private void ShowVideo() {
+    private void ShowVideo()
+    {
+        foreach (var VARIABLE in _enemies)
+        {
+            VARIABLE.gameObject.SetActive(false);
+        }
+
+        FindObjectOfType<Naruto>().GetComponentInChildren<APRController>().useControls = false;
+
         _isPlaying = true;
     }
 
-    void EndReached(UnityEngine.Video.VideoPlayer vp) {
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
         FinishEvent?.Invoke();
+
+        foreach (var VARIABLE in _enemies)
+        {
+            VARIABLE.gameObject.SetActive(true);
+        }
+
+        FindObjectOfType<Naruto>().GetComponentInChildren<APRController>().useControls = true;
+        _isPlaying = false;
+        Invoke(nameof(TurnOn), 1f);
     }
 }
